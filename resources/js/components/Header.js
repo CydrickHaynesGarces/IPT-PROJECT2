82fb8b31f085f8400a61a77f378805391a846b3b
Cydrick Haynes Garces <haynescydrick@gmail.com>
2024-09-28 19:38:49 +0800

    new added features

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,17 +1,30 @@
-// resources/js/components/Header.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../sass/Header.scss';
-import SocialMediaLinks from './SocialMediaLinks'; // Import SocialMediaLinks
+import SocialMediaLinks from './SocialMediaLinks';
 
 export default function Header() {
+    const [backgroundOpacity, setBackgroundOpacity] = useState(1); // For background opacity
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        const newOpacity = Math.max(1 - scrollY / 300, 0); // Decrease opacity to a minimum of 0
+        setBackgroundOpacity(newOpacity);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="header">
+        <header className="header" style={{ backgroundColor: `rgba(0, 0, 0, ${backgroundOpacity})` }}>
             <div className="header-content">
                 <div className="header-text">
                     <h1>IPT-PROJECT SYSTEM</h1>
                     <p>Layo na pero layo pagyud T T.</p>
                 </div>
-                {/* Add the SocialMediaLinks component here */}
                 <SocialMediaLinks />
             </div>
         </header>
